81e3f686a2aacb50d2b6bd23edb4f71526f26c96
Tynan Barlow <tynan@noukod.org>
2018-09-03 21:57:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
 
   puts " #{board[0]} ""|"" #{board[1]} ""|"" #{board[2]} "
@@ -7,5 +8,3 @@ def display_board(board)
   puts " #{board[6]} ""|"" #{board[7]} ""|"" #{board[8]} "
 
 end
-
-display_board(board)
